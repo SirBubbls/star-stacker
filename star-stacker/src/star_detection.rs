4b00e4fe64f06ff3a144a217c9f578eb304dfecd
SirBubbls <lucassas@live.de>
2021-12-24 13:20:22 +0100

    feat: support for image stacking

---
+++ star-stacker/src/star_detection.rs
@@ -0,0 +1,34 @@
+use opencv::prelude::*;
+use opencv::core::{Scalar, Mat, Vector, KeyPoint};
+use log::debug;
+
+
+pub fn detect_keypoints(image: &Mat) -> Vector<KeyPoint> {
+    let mut keypoints = Vector::<KeyPoint>::default();
+
+    let mut star_detector = opencv::xfeatures2d::StarDetector::create(
+            10,
+            28,
+            10,
+            8,
+            5
+    ).unwrap();
+
+    star_detector.detect(&image, &mut keypoints, &opencv::core::Mat::default())
+                 .expect("Unable to detect stars from image.");
+
+    debug!("Found {} keypoints", keypoints.len());
+    keypoints
+}
+
+pub fn draw_keypoints(image: &Mat, keypoints: &Vector<KeyPoint>) -> Mat {
+    let overlay_color = Scalar::new(0.0, 0.0, 255.0, 0.0);
+    let mut overlay_image = image.clone();
+
+    opencv::features2d::draw_keypoints(&image,
+                                       keypoints,
+                                       &mut overlay_image,
+                                       overlay_color,
+                                       opencv::features2d::DrawMatchesFlags::DRAW_RICH_KEYPOINTS).unwrap();
+    overlay_image
+}
---
+++ star-stacker/tests/image_loading.rs
@@ -0,0 +1,25 @@
+use std::fs::File;
+use std::path::Path;
+use rawloader::RawImage;
+use std::slice::Chunks;
+use std::fmt::Binary;
+use tiff::decoder::Decoder;
+
+
+#[test]
+fn load_dng_v6_6() {
+    let loader = rawloader::RawLoader::new();
+    let mut file = File::open(Path::new("tests/resources/set_1/x_x.dng")).unwrap();
+    let image: RawImage = loader.decode(&mut file, false).unwrap();
+    // println!("Loaded Image Dims: [{}, {}]", data.width, data.height);
+
+    if let rawloader::RawImageData::Integer(x) = image.data {
+        x.as_slice().chunks(3).take(5).for_each(|x| {
+            println!("{}", x.iter().map(|x| {
+                format!("{:b}", x)
+            }).collect::<Vec<String>>().join(" ")
+        )});
+    };
+}
+
+
